314d9e7da02710d17f944c44f3b9cade6bcbeff0
[leandro] <leandro.ls.silva.santos@gmail.com>
2024-10-19 16:39:44 -0300

    commit inicial

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        título: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'Leandro Santos',
+                partido: 'SFC',
+                fotos:[
+                    {url:'', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'Raffaella Geraldelli',
+                partido: 'ABC',
+                fotos:[
+                    {url:'', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        título: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 50,
+                name: 'Guilherme Boulos',
+                partido: 'SFC',
+                vice: 'Marta Suplicy',
+                fotos:[
+                    {url:'', legenda: 'Prefeito'},
+                    {url:'', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 15,
+                name: 'Ricardo Nunes',
+                partido: 'ABC',
+                vice: 'Ricardo Mello',
+                fotos:[
+                    {url:'', legenda: 'Vereador'},
+                    {url:'', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+]
